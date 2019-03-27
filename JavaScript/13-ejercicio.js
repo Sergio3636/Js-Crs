f47ce155b7f97c4965d23f321bf72512e925e1c8
Sergio <licanqueosergio1@gmail.com>
2019-03-26 22:44:50 -0300

    Ejercicios Basicos
    document.write
    y concatenar con "+" en mensajes y variables

--- JavaScript/13-ejercicio.js
+++ JavaScript/13-ejercicio.js
@@ -1,2 +1,10 @@
 'use strict'
-var numero = parseInt(prompt("INGRESE UN NUMERO",0));
+var numero1 = parseInt(prompt("INGRESE UN NUMERO",0));
+var numero2 = parseInt(prompt("INGRESE EL NUMERO 2"));
+
+//IMPORTANTE CONCATENAR CON "+"
+// contador de  numeros intermedios
+document.write("<h1> De "+numero1 +" a " +numero2+ " hay estos Numeros</h1>");
+for(var i = numero1 ; i <= numero2 ; i++){
+    document.write(i+"<br>");
+}
